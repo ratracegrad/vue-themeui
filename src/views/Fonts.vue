e498b6ec6ddf255fd738080593d4360491f55b97
Jennifer Bland <jennifer.bland@voxmedia.com>
2020-03-24 12:11:20 -0400

    Updated route

--- src/views/Fonts.vue
+++ src/views/Fonts.vue
@@ -55,7 +55,7 @@
           </defs>
         </svg>
         <ul>
-          <li><router-link to="/home">Intro</router-link></li>
+          <li><router-link to="/">Intro</router-link></li>
           <li><router-link to="/fonts" class="active">Fonts</router-link></li>
           <li><router-link to="/categories">Type categories</router-link></li>
           <li><router-link to="/typography">Typography</router-link></li>
