2880e911eb91b8b8e3f1310ef598fc5799edb594
Jennifer Bland <jennifer.bland@voxmedia.com>
2020-03-26 19:07:15 -0400

    Updated typography so that it changes the text dynamically

--- src/views/Typography.vue
+++ src/views/Typography.vue
@@ -9,10 +9,14 @@
       <h3>Line Heights</h3>
       <div class="flex margin-lg">
         <res-input
-          ><input type="text" placeholder="1.1" /><label
-            >Tight</label
-          ></res-input
-        ><span style="font-size:24px;line-height:1.1;"
+          ><input
+            type="text"
+            :placeholder="lineHeight.tight"
+            v-model="lineHeight.tight"
+          /><label>Tight</label></res-input
+        ><span
+          style="font-size:24px;"
+          v-bind:style="{ lineHeight: lineHeight.tight }"
           >This line height applies to larger font sizes, such as those used in
           headlines. Here's some sample text for you to test this value. Let's
           use this example text to also tell the user how this works</span
@@ -20,10 +24,14 @@
       </div>
       <div class="flex margin-lg">
         <res-input
-          ><input type="text" placeholder="1.3" /><label
-            >Medium</label
-          ></res-input
-        ><span style="font-size:16px;line-height:1.3;"
+          ><input
+            type="text"
+            :placeholder="lineHeight.medium"
+            v-model="lineHeight.medium"
+          /><label>Medium</label></res-input
+        ><span
+          style="font-size:16px;"
+          v-bind:style="{ lineHeight: lineHeight.medium }"
           >This line height applies to medium font sizes. Here's some sample
           text for you to test this value. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
@@ -32,10 +40,14 @@
       </div>
       <div class="flex margin-lg">
         <res-input
-          ><input type="text" placeholder="1.5" /><label
-            >Loose</label
-          ></res-input
-        ><span style="font-size:12px;line-height:1.5;"
+          ><input
+            type="text"
+            :placeholder="lineHeight.loose"
+            v-model="lineHeight.loose"
+          /><label>Loose</label></res-input
+        ><span
+          style="font-size:12px;"
+          v-bind:style="{ lineHeight: lineHeight.loose }"
           >This line height applies to small font sizes. Here's some sample text
           for you to test this value. Lorem ipsum dolor sit amet, consectetur
           adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
@@ -44,8 +56,14 @@
       </div>
       <div class="flex margin-lg">
         <res-input
-          ><input type="text" placeholder="1.7" /><label>Body</label></res-input
-        ><span style="font-size:16px;line-height:1.7;"
+          ><input
+            type="text"
+            :placeholder="lineHeight.body"
+            v-model="lineHeight.body"
+          /><label>Body</label></res-input
+        ><span
+          style="font-size:16px;"
+          v-bind:style="{ lineHeight: lineHeight.body }"
           >This line height applies to the body copy in an article, which we
           think is a little different than other text. Here's some sample text
           for you to test this value.</span
@@ -54,55 +72,66 @@
       <h3>Letterspacing</h3>
       <div class="flex margin-lg">
         <res-input
-          ><input type="text" placeholder="0" /><label
-            >Default</label
-          ></res-input
-        ><span style="font-size:16px;letter-spacing:0;"
-          >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.</span
-        >
-      </div>
-      <div class="flex margin-lg">
-        <res-input
-          ><input type="text" placeholder=".04em" /><label
-            >Caps</label
-          ></res-input
+          ><input
+            type="text"
+            :placeholder="letterSpacing.default"
+            v-model="letterSpacing.default"
+          /><label>Default</label></res-input
         ><span
-          style="font-size:12px;letter-spacing:.04em;text-transform:uppercase;"
+          style="font-size:16px;"
+          v-bind:style="{ letterSpacing: letterSpacing.default }"
           >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.</span
         >
       </div>
       <div class="flex margin-lg">
         <res-input
-          ><input type="text" placeholder=".02em" /><label
-            >Expressive</label
-          ></res-input
-        ><span style="font-size:16px;letter-spacing:.02em;"
+          ><input
+            type="text"
+            :placeholder="letterSpacing.caps"
+            v-model="letterSpacing.caps"
+          /><label>Caps</label></res-input
+        ><span
+          style="font-size:12px;text-transform:uppercase;"
+          v-bind:style="{ letterSpacing: letterSpacing.caps }"
           >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.</span
         >
       </div>
-    </div>
-
-    <div>
-      <router-link to="#" style="text-decoration: none;"
-        ><res-button design="primary"
-          ><button>Save</button></res-button
-        ></router-link
-      >
-      <router-link to="/lora" style="text-decoration: none;"
-        ><res-button design="secondary"
-          ><button>Next</button></res-button
-        ></router-link
-      >
+      <div class="flex margin-lg">
+        <res-input
+          ><input
+            type="text"
+            :placeholder="letterSpacing.expressive"
+            v-model="letterSpacing.expressive"
+          /><label>Expressive</label></res-input
+        ><span
+          style="font-size:16px;"
+          v-bind:style="{ letterSpacing: letterSpacing.expressive }"
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.</span
+        >
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: "Typography"
+  name: "Typography",
+  data: () => ({
+    lineHeight: {
+      tight: 1.1,
+      medium: 1.3,
+      loose: 1.5,
+      body: 1.7
+    },
+    letterSpacing: {
+      default: "0",
+      caps: ".04em",
+      expressive: ".02em"
+    }
+  })
 };
 </script>
 
