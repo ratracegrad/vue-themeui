d4fc697c59e49107aec437afabd34f905313d3a7
Jennifer Bland <jennifer.bland@voxmedia.com>
2020-03-26 14:05:24 -0400

    Added components to visualize in the stepper

--- src/views/Menu2.vue
+++ src/views/Menu2.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-stepper v-model="e1">
+    <v-stepper alt-labels v-model="e1">
       <v-stepper-header>
         <v-stepper-step :complete="e1 > 1" step="1">Intro</v-stepper-step>
 
@@ -29,7 +29,9 @@
 
       <v-stepper-items>
         <v-stepper-content step="1">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+          <!--          <v-card class="mb-12" color="grey lighten-1" height="200px">-->
+          <!--          </v-card>-->
+          <Intro></Intro>
           <v-toolbar color="transparent" flat>
             <v-btn color="primary" @click="e1 = 2">
               Continue
@@ -40,7 +42,8 @@
         </v-stepper-content>
 
         <v-stepper-content step="2">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+          <!--          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>-->
+          <Fonts></Fonts>
           <v-toolbar color="transparent" flat>
             <v-btn color="primary" @click="e1 = 3">
               Continue
@@ -51,7 +54,8 @@
         </v-stepper-content>
 
         <v-stepper-content step="3">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+          <!--          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>-->
+          <Categories></Categories>
           <v-toolbar color="transparent" flat>
             <v-btn color="primary" @click="e1 = 4">
               Continue
@@ -62,7 +66,8 @@
         </v-stepper-content>
 
         <v-stepper-content step="4">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+          <!--          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>-->
+          <Typography></Typography>
           <v-toolbar color="transparent" flat>
             <v-btn color="primary" @click="e1 = 5">
               Continue
@@ -73,7 +78,8 @@
         </v-stepper-content>
 
         <v-stepper-content step="5">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+          <!--          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>-->
+          <Sizing></Sizing>
           <v-toolbar color="transparent" flat>
             <v-btn color="primary" @click="e1 = 6">
               Continue
@@ -84,7 +90,8 @@
         </v-stepper-content>
 
         <v-stepper-content step="6">
-          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+          <!--          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>-->
+          <Spacing></Spacing>
           <v-toolbar color="transparent" flat>
             <v-btn color="primary" @click="e1 = 1">
               Continue
@@ -99,12 +106,27 @@
 </template>
 
 <script>
+import Intro from "@/components/Intro";
+import Fonts from "@/components/Fonts";
+import Categories from "@/components/Categories";
+import Typography from "@/components/Typography";
+import Sizing from "@/components/Sizing";
+import Spacing from "@/components/Spacing";
+
 export default {
   name: "Menu2",
   data() {
     return {
       e1: 1
     };
+  },
+  components: {
+    Intro,
+    Fonts,
+    Categories,
+    Typography,
+    Sizing,
+    Spacing
   }
 };
 </script>
