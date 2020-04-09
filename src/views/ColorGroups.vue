e121670022307f178f92904db46c8d7e8d1191af
Jennifer Bland <jennifer.bland@voxmedia.com>
2020-04-09 10:08:08 -0400

    Added new pages for items in menu that have not been completed it

---
+++ src/views/ColorGroups.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="l-content">
+        <h2>Color Groups Page</h2>
+        <div class="c-buttons c-buttons-save">
+            <res-button design="primary" style="margin-right: 8px"
+                ><button>Save</button></res-button
+            >
+            <div>
+                <router-link
+                    to="/palette"
+                    style="text-decoration: none; margin-right: 8px"
+                >
+                    <res-button design="secondary"
+                        ><button>Back</button></res-button
+                    >
+                </router-link>
+                <router-link to="/meta" style="text-decoration: none;">
+                    <res-button design="secondary"
+                        ><button disabled>Next</button></res-button
+                    >
+                </router-link>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "ColorGroups"
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Meta.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="l-content">
+        <h2>Meta Page</h2>
+        <div class="c-buttons c-buttons-save">
+            <res-button design="primary" style="margin-right: 8px"
+                ><button>Save</button></res-button
+            >
+            <div>
+                <router-link
+                    to="/body"
+                    style="text-decoration: none; margin-right: 8px"
+                >
+                    <res-button design="secondary"
+                        ><button>Back</button></res-button
+                    >
+                </router-link>
+                <router-link to="/ui" style="text-decoration: none;">
+                    <res-button design="secondary"
+                        ><button>Next</button></res-button
+                    >
+                </router-link>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Meta"
+};
+</script>
+
+<style scoped></style>
