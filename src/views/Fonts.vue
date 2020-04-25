800724edbb2115cf0ae4eca38cd788d480a48938
Jennifer Bland <jennifer.bland@voxmedia.com>
2020-04-25 17:52:51 -0400

    Updated the font section to all user to select one or more fonts to be uploaded. It will show font name and then an upload font button

--- src/views/Fonts.vue
+++ src/views/Fonts.vue
@@ -43,233 +43,93 @@
                     novalidate
                     v-if="isInitial || isSaving"
                 >
-                    <h1>Upload fonts</h1>
-                    <div class="dropbox">
-                        <input
-                            type="file"
-                            :name="uploadFieldName"
-                            :disabled="isSaving"
-                            id="fontFile"
-                            @change="
-                                filesChange(
-                                    $event.target.name,
-                                    $event.target.files
-                                );
-                                fileCount = $event.target.files.length;
-                            "
-                            accept="font*"
-                        />
-                        <p v-if="isInitial">
-                            Drag your file(s) here to begin<br />
-                            or click to browse
-                        </p>
-                        <p v-if="isSaving">Uploading {{ fileCount }} files</p>
+                    <div class="uploadWrapper">
+                        <label class="fileContainer">
+                            Select Fonts
+                            <input
+                                type="file"
+                                :name="uploadFieldName"
+                                :disabled="isSaving"
+                                id="fontFile"
+                                ref="file"
+                                multiple
+                                accept=".ttf,.woff,.woff2,.otf"
+                                @change="getFiles"
+                            />
+                        </label>
                     </div>
                 </form>
-
-                <!--SUCCESS-->
-                <div v-if="isSuccess">
-                    <h2>
-                        Uploaded {{ uploadedFiles.length }} file(s)
-                        successfully.
-                    </h2>
-                    <p>
-                        <a href="javascript:void(0)" @click="reset()"
-                            >Upload again</a
-                        >
-                    </p>
-                    <ul class="list-unstyled">
-                        <li v-for="(item, idx) in uploadedFiles" :key="idx">
-                            <!--              <img :src="item.url" class="img-responsive img-thumbnail" :alt="item.originalName">-->
-                            {{ item.originalname }} - {{ item.path }}
-                        </li>
-                    </ul>
-                </div>
-                <!--FAILED-->
-                <div v-if="isFailed">
-                    <h2>Uploaded failed.</h2>
-                    <p>
-                        <a href="javascript:void(0)" @click="reset()"
-                            >Try again</a
-                        >
-                    </p>
-                    <pre>{{ uploadError }}</pre>
-                </div>
             </div>
 
-            <div v-show="radioChoice === 'URL'">
-                <res-input>
-                    <label
-                        >Include from URL (url from Google, Hoefler, Typekit,
-                        etc)</label
-                    >
-                    <input type="file" />
-                    <res-button slot="outer-right" design="primary"
-                        ><button>Submit</button></res-button
-                    >
-                </res-input>
+            <!--            <div v-show="radioChoice === 'URL'">-->
+            <!--                <res-input>-->
+            <!--                    <label-->
+            <!--                        >Include from URL (url from Google, Hoefler, Typekit,-->
+            <!--                        etc)</label-->
+            <!--                    >-->
+            <!--                    <input type="file" />-->
+            <!--                    <res-button slot="outer-right" design="primary"-->
+            <!--                        ><button>Submit</button></res-button-->
+            <!--                    >-->
+            <!--                </res-input>-->
+            <!--            </div>-->
+        </div>
+
+        <div v-if="totalSize > 0">
+            <hr />
+            <div style="font-size: 24px;">
+                <p style="font-size: 12px;">
+                    <i>Fonts below illustrated uploaded fonts, not defaults</i>
+                </p>
+
+                <div>
+                    <p v-for="(file, fileIdx) in files" :key="fileIdx">
+                        {{ file.fontName }} - ({{ file.fileSize | kb }})
+                        <res-icon
+                            name="x-circle"
+                            size="small"
+                            style="vertical-align: middle;"
+                            @click="removeFile(file)"
+                        ></res-icon>
+                    </p>
+                </div>
+            </div>
+            <div class="totals">
+                <strong>Total File Size: {{ totalSize | kb }} </strong>
+                <span>&nbsp;</span>
+                <res-icon
+                    id="medium-font-load-tooltip"
+                    name="info"
+                    size="small"
+                    style="vertical-align: text-bottom;"
+                ></res-icon>
+                <span>&nbsp;</span>
+                <res-badge size="medium" :design="loadDesign"
+                    >Load Time: {{ loadTime }}</res-badge
+                >
+            </div>
+            <res-dropdown for="medium-font-load-tooltip"
+                >file size range for "medium" is tktk</res-dropdown
+            >
+            <div>
+                <res-button design="secondary">
+                    <res-icon name="upload"></res-icon><button>Upload Fonts</button></res-button>
             </div>
         </div>
 
         <hr />
-
-        <div style="font-size: 24px;">
-            <p style="font-size: 12px;">
-                <i>Fonts below illustrated uploaded fonts, not defaults</i>
-            </p>
-            <p style="font-family: Graphik; font-weight: 400;">
-                Graphik-Regular
-                <res-icon
-                    name="x-circle"
-                    size="small"
-                    res-modal-open="modal1"
-                    style="vertical-align: middle;"
-                ></res-icon>
-            </p>
-
-            <res-modal id="modal1" close-on-bg-click="">
-                <div>
-                    <p style="margin-bottom: 1rem;">Delete Graphik-Regular?</p>
-                    <res-button
-                        id="modal-close"
-                        design="secondary"
-                        res-modal-close="modal1"
-                        ><button>Delete Font</button></res-button
-                    >
-                    <res-button
-                        id="modal-close"
-                        res-modal-close="modal1"
-                        style="margin-left: 1rem;"
-                    >
-                        <res-icon name="x"></res-icon
-                        ><button>Cancel</button></res-button
-                    >
-                </div>
-            </res-modal>
-
-            <p class="graphik-bold">
-                Graphik-Bold
-                <res-icon
-                    name="x-circle"
-                    size="small"
-                    style="vertical-align: middle;"
-                    res-modal-open="modal2"
-                ></res-icon>
-            </p>
-
-            <res-modal id="modal2" close-on-bg-click="">
-                <div>
-                    <p style="margin-bottom: 1rem;">Delete Graphik-Bold?</p>
-                    <res-button
-                        id="modal-close"
-                        design="secondary"
-                        res-modal-close="modal2"
-                        ><button>Delete Font</button></res-button
-                    >
-                    <res-button
-                        id="modal-close"
-                        res-modal-close="modal2"
-                        style="margin-left: 1rem;"
-                    >
-                        <res-icon name="x"></res-icon
-                        ><button>Cancel</button></res-button
-                    >
-                </div>
-            </res-modal>
-
-            <p class="lora-regular">
-                Lora
-                <res-icon
-                    name="x-circle"
-                    size="small"
-                    style="vertical-align: middle;"
-                    res-modal-open="modal3"
-                ></res-icon>
-            </p>
-
-            <res-modal id="modal3" close-on-bg-click="">
-                <div>
-                    <p style="margin-bottom: 1rem;">Delete Lora?</p>
-                    <res-button
-                        id="modal-close"
-                        design="secondary"
-                        res-modal-close="modal3"
-                        ><button>Delete Font</button></res-button
-                    >
-                    <res-button
-                        id="modal-close"
-                        res-modal-close="modal3"
-                        style="margin-left: 1rem;"
-                    >
-                        <res-icon name="x"></res-icon
-                        ><button>Cancel</button></res-button
-                    >
-                </div>
-            </res-modal>
-
-            <p class="lora-bold">
-                Lora-Bold
-                <res-icon
-                    name="x-circle"
-                    size="small"
-                    style="vertical-align: middle;"
-                    res-modal-open="modal4"
-                ></res-icon>
-            </p>
-
-            <res-modal id="modal4" close-on-bg-click="">
-                <div>
-                    <p style="margin-bottom: 1rem;">Delete Lora-Bold?</p>
-                    <res-button
-                        id="modal-close"
-                        design="secondary"
-                        res-modal-close="modal4"
-                        ><button>Delete Font</button></res-button
-                    >
-                    <res-button
-                        id="modal-close"
-                        res-modal-close="modal4"
-                        style="margin-left: 1rem;"
-                    >
-                        <res-icon name="x"></res-icon
-                        ><button>Cancel</button></res-button
-                    >
-                </div>
-            </res-modal>
-        </div>
-        <p>
-            <strong>Total File Size: 200kb</strong>
-            <res-icon
-                id="medium-font-load-tooltip"
-                name="info"
-                size="small"
-                style="vertical-align: text-bottom;"
-            ></res-icon>
-        </p>
-        <res-dropdown for="medium-font-load-tooltip"
-            >file size range for "medium" is tktk</res-dropdown
-        >
-        <div>
-            <res-badge size="medium" design="gold">Load Time: Medium</res-badge>
-        </div>
-
-        <hr />
-
         <PrevNext :prevLink="'index'" :nextLink="'typesizing'"></PrevNext>
     </section>
 </template>
 
 <script>
-import * as axios from 'axios';
 import PrevNext from '@/components/PrevNext';
-
-// const BASE_URL = "http://localhost:5000";
-const BASE_URL = 'https://themui-backend.herokuapp.com';
+// const BASE_URL = 'https://themui-backend.herokuapp.com';
 const STATUS_INITIAL = 0,
-    STATUS_SAVING = 1,
-    STATUS_SUCCESS = 2,
-    STATUS_FAILED = 3;
-
+    STATUS_SAVING = 1;
+// STATUS_SUCCESS = 2,
+// STATUS_FAILED = 3;
+import openType from 'opentype.js';
 export default {
     name: 'Fonts',
     components: {
@@ -277,10 +137,11 @@ export default {
     },
     data: () => ({
         radioChoice: null,
-        uploadedFiles: [],
         uploadError: null,
         currentStatus: null,
-        uploadFieldName: 'fonts'
+        uploadFieldName: 'fonts',
+        files: [],
+        fileTypes: ['otf', 'ttf', 'woff']
     }),
     computed: {
         isInitial() {
@@ -289,75 +150,62 @@ export default {
         isSaving() {
             return this.currentStatus === STATUS_SAVING;
         },
-        isSuccess() {
-            return this.currentStatus === STATUS_SUCCESS;
+        totalSize() {
+            return this.files.reduce((accum, item) => accum + item.fileSize, 0);
         },
-        isFailed() {
-            return this.currentStatus === STATUS_FAILED;
+        loadTime() {
+            return this.totalSize <= 200000
+                ? 'fast'
+                : this.totalSize <= 400000
+                ? 'moderate'
+                : 'slow';
+        },
+        loadDesign() {
+            return this.totalSize <= 200000
+                ? 'grass'
+                : this.totalSize <= 400000
+                ? 'gold'
+                : 'apple';
         }
     },
     methods: {
         reset() {
-            // reset form to initial state
             this.currentStatus = STATUS_INITIAL;
-            this.uploadedFiles = [];
             this.uploadError = null;
+            this.files = [];
+            this.totalSize = 0;
         },
-        save(formData) {
-            // upload data to the server
-            this.currentStatus = STATUS_SAVING;
-
-            this.upload(formData)
-                .then(x => {
-                    this.uploadedFiles = [].concat(x);
-                    this.currentStatus = STATUS_SUCCESS;
-                })
-                .catch(err => {
-                    this.uploadError = err.response;
-                    this.currentStatus = STATUS_FAILED;
-                });
+        getFiles(e) {
+            const fonts = e.target.files;
+            if (!fonts) return;
+            [...fonts].forEach(f => {
+                this.processFile(f);
+            });
         },
-        filesChange(fieldName, fileList) {
-            // handle file changes
-            const formData = new FormData();
-
-            if (!fileList.length) return;
-
-            // append the files to FormData
-            // Array.from(Array(fileList.length).keys()).map(x => {
-            //   console.log(fieldName, fileList[x], fileList[x].name);
-            //   formData.append(fieldName, fileList[x], fileList[x].name);
-            // });
-            formData.append('file', fileList[0]);
-
-            // save it
-            this.save(formData);
+        processFile(f) {
+            let reader = new FileReader();
+            let n = this;
+            reader.onload = function(e) {
+                let font = openType.parse(e.target.result);
+                if (font.supported) {
+                    n.files.push({
+                        fontName: font.names.fullName.en,
+                        fileSize: f.size,
+                        fileName: f.name,
+                        file: f,
+                        uploaded: false
+                    });
+                }
+            };
+            reader.onerror = function(e) {
+                console.log('error', e);
+            };
+            reader.readAsArrayBuffer(f);
         },
-        upload(formData) {
-            const url = `${BASE_URL}/upload`;
-            // console.log(`url: ${url}`);
-            return (
-                axios
-                    .post(url, formData)
-                    // get data
-                    .then(x => {
-                        // console.log("------ x");
-                        // console.log(x);
-                        return x.data;
-                    })
-                    // add url field
-                    .then(x => {
-                        // console.log("2nd then");
-                        return Object.assign({}, x.file, {
-                            url: `${BASE_URL}/uploads/${x.filename}`
-                        });
-                        // x.map(img => {
-                        //   console.log('===== img');
-                        //   console.log(img);
-                        //   return Object.assign({}, img, { url: `${BASE_URL}/upload/${img.id}` })
-                        // })
-                    })
-            );
+        removeFile(file) {
+            this.files = this.files.filter(f => {
+                return f != file;
+            });
         }
     },
     mounted() {
@@ -367,32 +215,46 @@ export default {
 </script>
 
 <style scoped>
-.dropbox {
-    outline: 2px dashed grey; /* the dash box */
-    outline-offset: -10px;
-    background: lightcyan;
-    color: dimgray;
-    padding: 10px 10px;
-    min-height: 200px; /* minimum height */
+.fileContainer {
+    overflow: hidden;
     position: relative;
-    cursor: pointer;
 }
 
-.input-file {
-    opacity: 0; /* invisible but it's there! */
-    width: 100%;
-    height: 200px;
+.fileContainer [type='file'] {
+    cursor: inherit;
+    color: black;
+    display: block;
+    font-size: 999px;
+    filter: alpha(opacity=0);
+    min-height: 100%;
+    min-width: 100%;
+    opacity: 0;
     position: absolute;
+    right: 0;
+    text-align: right;
+    top: 0;
+}
+.fileContainer {
+    background: #ffe068;
+    border-radius: 5px;
+    float: left;
+    padding: 10px 17px;
+}
+.fileContainer [type='file'] {
     cursor: pointer;
 }
-
-.dropbox:hover {
-    background: lightblue; /* when mouse over to the drop zone, change color */
+.uploadWrapper {
+    margin: 25px 0;
+    min-height: 25px;
 }
-
-.dropbox p {
-    font-size: 1.2em;
-    text-align: center;
-    padding: 50px 0;
+.totals {
+    display: flex;
+    align-items: center;
+}
+.totals res-icon {
+    margin-left: 5px;
+}
+.totals res-badge {
+    margin-left: 10px;
 }
 </style>
