d84a041923c7f2ab26dba3b30e2409d979c1e18a
Jennifer Bland <jennifer.bland@voxmedia.com>
2020-04-30 17:42:47 -0400

    Added default values for the small, medium and large type sizing

--- src/store/index.js
+++ src/store/index.js
@@ -11,50 +11,149 @@ export default new Vuex.Store({
             moderate: 1000000
         },
         fonts: [],
-        sizes: {},
-        characterSpacing: {},
-        categories: {},
-        headings: {},
-        body: {},
-        meta: {},
-        UI: {},
-        spacing: {},
-        palette: {},
-        colorGroups: {}
+        currentScale: 'small',
+        sizes: {
+            small: {
+                phone: {
+                    'font-size-01': '12px',
+                    'font-size-02': '14px',
+                    'font-size-03': '16px',
+                    'font-size-04': '18px',
+                    'font-size-05': '20px',
+                    'font-size-06': '22px',
+                    'font-size-07': '26px',
+                    'font-size-08': '35px',
+                    'font-size-09': '45px',
+                    'font-size-10': '64px',
+                    'font-size-11': '78px',
+                    'font-size-12': '136px'
+                },
+                tablet: {
+                    'font-size-01': '12px',
+                    'font-size-02': '14px',
+                    'font-size-03': '16px',
+                    'font-size-04': '18px',
+                    'font-size-05': '20px',
+                    'font-size-06': '22px',
+                    'font-size-07': '26px',
+                    'font-size-08': '35px',
+                    'font-size-09': '45px',
+                    'font-size-10': '64px',
+                    'font-size-11': '78px',
+                    'font-size-12': '136px'
+                },
+                desktop: {
+                    'font-size-01': '12px',
+                    'font-size-02': '14px',
+                    'font-size-03': '16px',
+                    'font-size-04': '18px',
+                    'font-size-05': '20px',
+                    'font-size-06': '22px',
+                    'font-size-07': '26px',
+                    'font-size-08': '35px',
+                    'font-size-09': '45px',
+                    'font-size-10': '64px',
+                    'font-size-11': '78px',
+                    'font-size-12': '136px'
+                }
+            },
+            medium: {
+                phone: {
+                    'font-size-01': '13px',
+                    'font-size-02': '14px',
+                    'font-size-03': '16px',
+                    'font-size-04': '18px',
+                    'font-size-05': '20px',
+                    'font-size-06': '24px',
+                    'font-size-07': '35px',
+                    'font-size-08': '44px',
+                    'font-size-09': '54px',
+                    'font-size-10': '86px',
+                    'font-size-11': '104px',
+                    'font-size-12': '136px'
+                },
+                tablet: {
+                    'font-size-01': '13px',
+                    'font-size-02': '14px',
+                    'font-size-03': '16px',
+                    'font-size-04': '18px',
+                    'font-size-05': '20px',
+                    'font-size-06': '24px',
+                    'font-size-07': '35px',
+                    'font-size-08': '44px',
+                    'font-size-09': '54px',
+                    'font-size-10': '86px',
+                    'font-size-11': '104px',
+                    'font-size-12': '136px'
+                },
+                desktop: {
+                    'font-size-01': '13px',
+                    'font-size-02': '14px',
+                    'font-size-03': '16px',
+                    'font-size-04': '18px',
+                    'font-size-05': '20px',
+                    'font-size-06': '24px',
+                    'font-size-07': '35px',
+                    'font-size-08': '44px',
+                    'font-size-09': '54px',
+                    'font-size-10': '86px',
+                    'font-size-11': '104px',
+                    'font-size-12': '136px'
+                }
+            },
+            desktop: {
+                phone: {
+                    'font-size-01': '13px',
+                    'font-size-02': '14px',
+                    'font-size-03': '16px',
+                    'font-size-04': '18px',
+                    'font-size-05': '20px',
+                    'font-size-06': '24px',
+                    'font-size-07': '35px',
+                    'font-size-08': '44px',
+                    'font-size-09': '54px',
+                    'font-size-10': '86px',
+                    'font-size-11': '104px',
+                    'font-size-12': '136px'
+                },
+                tablet: {
+                    'font-size-01': '13px',
+                    'font-size-02': '14px',
+                    'font-size-03': '16px',
+                    'font-size-04': '18px',
+                    'font-size-05': '20px',
+                    'font-size-06': '24px',
+                    'font-size-07': '35px',
+                    'font-size-08': '44px',
+                    'font-size-09': '54px',
+                    'font-size-10': '86px',
+                    'font-size-11': '104px',
+                    'font-size-12': '136px'
+                },
+                large: {
+                    'font-size-01': '13px',
+                    'font-size-02': '14px',
+                    'font-size-03': '16px',
+                    'font-size-04': '18px',
+                    'font-size-05': '20px',
+                    'font-size-06': '24px',
+                    'font-size-07': '35px',
+                    'font-size-08': '44px',
+                    'font-size-09': '54px',
+                    'font-size-10': '86px',
+                    'font-size-11': '104px',
+                    'font-size-12': '136px'
+                }
+            }
+        },
+        sizingText: 'Almost before we knew it, we had left the ground.'
     },
     mutations: {
-        saveSizes(state, bundle) {
-            this.state.sizes = bundle;
-        },
-        saveCharacterSpacing(state, bundle) {
-            this.state.characterSpacing = bundle;
-        },
-        saveCategories(state, bundle) {
-            this.state.categories = bundle;
-        },
-        saveHeadings(state, bundle) {
-            this.state.headings = bundle;
-        },
-        saveBody(state, bundle) {
-            this.state.body = bundle;
-        },
-        saveMeta(state, bundle) {
-            this.state.meta = bundle;
-        },
-        saveUI(state, bundle) {
-            this.state.UI = bundle;
-        },
-        saveSpacing(state, bundle) {
-            this.state.spacing = bundle;
-        },
-        savePalette(state, bundle) {
-            this.state.palette = bundle;
-        },
-        saveColorGroups(state, bundle) {
-            this.state.colorGroups = bundle;
-        },
         addFont(state, font) {
             state.fonts.push(font);
+        },
+        saveCurrentScale(state, currentScale) {
+            state.currentScale = currentScale;
         }
     },
     actions: {},
