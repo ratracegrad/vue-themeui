9b65db1804dc4835f3c94d969c995c16fae2de48
Jennifer Bland <jennifer.bland@voxmedia.com>
2020-05-01 11:55:00 -0400

    Added saving categories to store

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -6,9 +6,11 @@
             <res-select
                 ><label
                     >Headings
-                    <p>Currently: <strong>Graphik-Regular</strong></p></label
+                    <p>
+                        Currently: <strong>{{ categories.headings }}</strong>
+                    </p></label
                 >
-                <select name="s1" id="initial-id1"
+                <select name="s1" v-model="headings" id="initial-id1"
                     ><option placeholder="">Select font</option
                     ><option value="Graphik-Regular">Graphik-Regular</option
                     ><option value="Graphik-Bold">Graphik-Bold</option
@@ -22,9 +24,11 @@
             <res-select
                 ><label
                     >Body
-                    <p>Currently: <strong>Graphik-Regular</strong></p></label
+                    <p>
+                        Currently: <strong>{{ categories.body }}</strong>
+                    </p></label
                 >
-                <select name="s1" id="initial-id2"
+                <select name="s1" v-model="body" id="initial-id2"
                     ><option placeholder="">Select font</option
                     ><option value="Graphik-Regular">Graphik-Regular</option
                     ><option value="Graphik-Bold">Graphik-Bold</option
@@ -38,9 +42,11 @@
             <res-select
                 ><label
                     >Meta
-                    <p>Currently: <strong>Graphik-Regular</strong></p></label
+                    <p>
+                        Currently: <strong>{{ categories.meta }}</strong>
+                    </p></label
                 >
-                <select name="s1" id="initial-id3"
+                <select name="s1" v-model="meta" id="initial-id3"
                     ><option placeholder="">Select font</option
                     ><option value="Graphik-Regular">Graphik-Regular</option
                     ><option value="Graphik-Bold">Graphik-Bold</option
@@ -54,9 +60,11 @@
             <res-select
                 ><label
                     >UI
-                    <p>Currently: <strong>Graphik-Regular</strong></p></label
+                    <p>
+                        Currently: <strong>{{ categories.UI }}</strong>
+                    </p></label
                 >
-                <select name="s1" id="initial-id4"
+                <select name="s1" id="initial-id4" v-model="UI"
                     ><option placeholder="">Select font</option
                     ><option value="Graphik-Regular">Graphik-Regular</option
                     ><option value="Graphik-Bold">Graphik-Bold</option
@@ -66,21 +74,43 @@
             >
         </div>
 
-        <div class="c-buttons c-buttons-save">
-            <res-button design="primary" style="margin-right: 8px"
-                ><button>Save</button></res-button
-            >
-        </div>
+        <SaveButton :enabled="true" v-on:save="handleSave"></SaveButton>
+
         <PrevNext :prevLink="'typography'" :nextLink="'headings'"></PrevNext>
     </section>
 </template>
 
 <script>
+import SaveButton from '@/components/SaveButton';
 import PrevNext from '@/components/PrevNext';
+import { mapState } from 'vuex';
+
 export default {
     name: 'Categories',
     components: {
+        SaveButton,
         PrevNext
+    },
+    data: () => ({
+        headings: '',
+        body: '',
+        meta: '',
+        UI: ''
+    }),
+    computed: {
+        ...mapState({
+            categories: state => state.categories
+        })
+    },
+    methods: {
+        handleSave() {
+            this.$store.commit('saveCategories', {
+                headings: this.headings,
+                body: this.body,
+                meta: this.meta,
+                UI: this.UI
+            });
+        }
     }
 };
 </script>
