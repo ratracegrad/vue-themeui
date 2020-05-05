daec3bb27ea340dbb8c429af2a71ca857727a3a8
Ambika Castle <ambika.castle@voxmedia.com>
2020-05-05 15:22:03 -0700

    generates default colors

--- src/views/Palette.vue
+++ src/views/Palette.vue
@@ -18,9 +18,22 @@
         ></ColorCard>
 
         <div class="margin-lg">
-            <res-button v-on:click="addColor" design="default"
+            <res-button
+                v-on:click="isHiddenPicker = !isHiddenPicker"
+                design="default"
                 ><res-icon name="add"></res-icon
                 ><button>Add a color</button></res-button
+            >
+            <v-color-picker
+                v-if="!isHiddenPicker"
+                width="275"
+                @update:color="pickColor"
+            ></v-color-picker>
+            <res-button
+                v-if="!isHiddenPicker"
+                v-on:click="addColor"
+                design="secondary"
+                ><button>Generate Color</button></res-button
             >
         </div>
         <hr />
@@ -38,6 +51,7 @@
 import PrevNext from '@/components/PrevNext';
 import ColorCard from '@/components/ColorCard';
 import NeutralsCard from '@/components/NeutralsCard';
+import colorConvert from 'color-convert';
 
 let colorCounter = 1;
 
@@ -50,14 +64,20 @@ export default {
     },
     data: function() {
         return {
+            isHiddenPicker: true,
+            newColor: '',
+            newColorFaded: '',
+            newColorLight: '',
+            newColorBright: '',
+            newColorDark: '',
             cards: [
                 {
                     id: `color${colorCounter++}`,
                     data: {
                         main: '#FC6340',
                         faded: '#F7F3EF',
-                        light: '#FF7E61',
-                        bright: '#FFDBC2',
+                        light: '#FFDBC2',
+                        bright: '#FF7E61',
                         dark: '#E74F2D'
                     }
                 }
@@ -69,14 +89,15 @@ export default {
             let newColor = {
                 id: `color${colorCounter++}`,
                 data: {
-                    main: '#FC6340',
-                    faded: '#F7F3EF',
-                    light: '#FF7E61',
-                    bright: '#FFDBC2',
-                    dark: '#E74F2D'
+                    main: this.newColor,
+                    faded: this.newColorFaded,
+                    light: this.newColorLight,
+                    bright: this.newColorBright,
+                    dark: this.newColorDark
                 }
             };
             this.cards.push(newColor);
+            this.isHiddenPicker = true;
         },
         deleteColor: function(colorID) {
             let newList = [];
@@ -88,6 +109,88 @@ export default {
             }
 
             this.cards = newList;
+        },
+        pickColor: function(color) {
+            this.newColor = color.hex;
+
+            var newColorHSL = colorConvert.hex.hsl(this.newColor);
+            var newColorHue = newColorHSL[0];
+
+            // generate faded
+
+            var createFadedColorHex = function() {
+                var newFadedColorHSL = [];
+
+                var FADED_COLOR_SATURATION = 20;
+                var FADED_COLOR_LIGHTNESS = 95;
+
+                newFadedColorHSL.push(
+                    newColorHue,
+                    FADED_COLOR_SATURATION,
+                    FADED_COLOR_LIGHTNESS
+                );
+
+                return '#' + colorConvert.hsl.hex(newFadedColorHSL);
+            };
+
+            this.newColorFaded = createFadedColorHex();
+
+            // generate light
+
+            var createLightColorHex = function() {
+                var newLightColorHSL = [];
+
+                var LIGHT_COLOR_SATURATION = 50;
+                var LIGHT_COLOR_LIGHTNESS = 85;
+
+                newLightColorHSL.push(
+                    newColorHue,
+                    LIGHT_COLOR_SATURATION,
+                    LIGHT_COLOR_LIGHTNESS
+                );
+
+                return '#' + colorConvert.hsl.hex(newLightColorHSL);
+            };
+
+            this.newColorLight = createLightColorHex();
+
+            // generate bright
+
+            var createBrightColorHex = function() {
+                var newBrightColorHSL = [];
+
+                var BRIGHT_COLOR_SATURATION = 80;
+                var BRIGHT_COLOR_LIGHTNESS = 50;
+
+                newBrightColorHSL.push(
+                    newColorHue,
+                    BRIGHT_COLOR_SATURATION,
+                    BRIGHT_COLOR_LIGHTNESS
+                );
+
+                return '#' + colorConvert.hsl.hex(newBrightColorHSL);
+            };
+
+            this.newColorBright = createBrightColorHex();
+
+            // generate dark
+
+            var createDarkColorHex = function() {
+                var newDarkColorHSL = [];
+
+                var DARK_COLOR_SATURATION = 50;
+                var DARK_COLOR_LIGHTNESS = 50;
+
+                newDarkColorHSL.push(
+                    newColorHue,
+                    DARK_COLOR_SATURATION,
+                    DARK_COLOR_LIGHTNESS
+                );
+
+                return '#' + colorConvert.hsl.hex(newDarkColorHSL);
+            };
+
+            this.newColorDark = createDarkColorHex();
         }
     }
 };
